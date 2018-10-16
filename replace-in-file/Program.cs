a96084fa3bf93b2c89e77c0165659e86b75233c1
Ashkan Saeedi Mazdeh <ashkan_gc@yahoo.com>
2018-10-16 15:08:24 +0330

    contains the replace-in-file implementation

---
+++ replace-in-file/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace replace_in_file
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length < 4 || (args.Length - 1) % 3 != 0)
+            {
+                ShowHelp();
+                return 1;
+            }
+            string filePath = args[0];
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine($"The file at path {filePath} does not exist"); ;
+                return 1;
+            }
+
+            string content = File.ReadAllText(filePath);
+            StringBuilder sb = new StringBuilder(content);
+            ProcessingSet[] items = new ProcessingSet[(args.Length - 1) / 3];
+            int counter = 0;
+            for (int i = 1; i < args.Length - 2; i += 3)
+            {
+                string separator = args[i];
+                if (separator != "-set")
+                {
+                    Console.WriteLine("You should separate your placeholder and value pairs using -set\n type replace-in-file /? for the syntax.");
+                    return 1;
+                }
+                string placeHolder = args[i + 1];
+                string vallue = args[i + 2];
+                var item = new ProcessingSet { PlaceHolder = placeHolder, Value = vallue };
+                items[counter] = item;
+                counter++;
+            }
+            for (int i = 0; i < items.Length; ++i)
+            {
+                sb = sb.Replace(items[i].PlaceHolder, items[i].Value);
+            }
+            File.WriteAllText(filePath, sb.ToString());
+            return 0;
+        }
+
+        private static void ShowHelp()
+        {
+            Console.WriteLine("The correct syntax is replace-in-file \"file-path\" -set \"placeholer1\" \"value1\" -set \"placeholder2\" \"value2\" ...");
+        }
+    }
+
+    struct ProcessingSet
+    {
+        public string PlaceHolder { get; set; }
+        public string Value { get; set; }
+    }
+}
